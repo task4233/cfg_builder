605c2443e9bb62b4ae2018087e61a331fe9f2159
task4233 <29667656+task4233@users.noreply.github.com>
2023-01-04 17:10:39 +0900

    fix: path

--- src/main/java/dev/task4233/playground/controlFlowGraph/CFG.java
+++ src/main/java/dev/task4233/playground/controlFlowGraph/CFG.java
@@ -3,10 +3,8 @@ package dev.task4233.playground.controlFlowGraph;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.FilenameFilter;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -16,20 +14,6 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.concurrent.ConcurrentHashMap;
 
-import fj.F;
-import soot.jimple.InvokeExpr;
-import soot.jimple.Stmt;
-import soot.jimple.infoflow.InfoflowConfiguration;
-import soot.jimple.infoflow.InfoflowConfiguration.CallgraphAlgorithm;
-import soot.jimple.infoflow.android.InfoflowAndroidConfiguration;
-import soot.jimple.infoflow.android.SetupApplication;
-import soot.jimple.toolkits.callgraph.CallGraph;
-import soot.jimple.toolkits.callgraph.Edge;
-import soot.Body;
-import soot.Scene;
-import soot.SootClass;
-import soot.SootMethod;
-import soot.Unit;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CFG {
@@ -46,7 +30,7 @@ public class CFG {
 
     private List<Map<String, Integer>> apiFreqs = new ArrayList<>();
     private List<List<String>> apiSequences = new ArrayList<>();
-    private List<List<Integer>> apiSequenceIndices = new ArrayList();
+    private List<List<Integer>> apiSequenceIndices = new ArrayList<>();
     private Map<String, Integer> allApis = new LinkedHashMap<>();
 
     private String apkPath = System.getProperty("user.dir") + File.separator + "samples";
@@ -59,7 +43,7 @@ public class CFG {
         if (chunk_idx.isEmpty()) {
             return;
         }
-        
+
         chunk_i = Integer.valueOf(chunk_idx);
         apk_lb = chunk_range * chunk_i;
         apk_ub = chunk_range * (chunk_i + 1);
@@ -70,7 +54,7 @@ public class CFG {
         int allApkNum = benignApks.length + maliciousApks.length;
         apk_ub = (apk_ub > allApkNum) ? allApkNum : apk_ub;
 
-        for (int idx = apk_lb; idx<apk_ub; ++idx) {
+        for (int idx = apk_lb; idx < apk_ub; ++idx) {
             apiFreqs.add(new ConcurrentHashMap<String, Integer>());
             apiSequences.add(new ArrayList<>());
             apiSequenceIndices.add(new ArrayList<>());
@@ -85,22 +69,21 @@ public class CFG {
             public boolean accept(File file, String fileName) {
                 // ignore not endwith .apk and > 4MB, and .gitignore
                 List<String> blackList = Arrays.asList(
-                    ".gitignore", 
-                    "VirusShare_0de5c01c9e66be313970cc3af017f188",
-                    "VirusShare_1a4ed1ca65321659b139f9cba9c9cab4",
-                    "VirusShare_1407cd7c568576115204697fdbbdfa43",
-                    "VirusShare_29e52d2de1cc2011eb8eacf0392d389d",
-                    "VirusShare_2e94d4723b8e4217d6b39aa286e3d39e",
-                    "VirusShare_2ebf5292505317771bbb458c17667437",
-                    "VirusShare_37a46aec9aa86831faa3ddb6b05a05f8",
-                    "VirusShare_5361e076f1744c43dd65cda00bb89cc5",
-                    "VirusShare_6606e8adad40e3c5b0b8c347a38eb86b",
-                    "VirusShare_98eb1f31945f4cd97088cf9fbc49d03b",
-                    "VirusShare_aecb7c76cb497401be48459ff944f5fe",
-                    "VirusShare_bcb3026536783bc774a05d93bc2f6039",
-                    "VirusShare_c69d0d8b86bf3946ccbc011767b06919",
-                    "VirusShare_ff28b758f18030c14402e100dbb6987e"
-                );
+                        ".gitignore",
+                        "VirusShare_0de5c01c9e66be313970cc3af017f188",
+                        "VirusShare_1a4ed1ca65321659b139f9cba9c9cab4",
+                        "VirusShare_1407cd7c568576115204697fdbbdfa43",
+                        "VirusShare_29e52d2de1cc2011eb8eacf0392d389d",
+                        "VirusShare_2e94d4723b8e4217d6b39aa286e3d39e",
+                        "VirusShare_2ebf5292505317771bbb458c17667437",
+                        "VirusShare_37a46aec9aa86831faa3ddb6b05a05f8",
+                        "VirusShare_5361e076f1744c43dd65cda00bb89cc5",
+                        "VirusShare_6606e8adad40e3c5b0b8c347a38eb86b",
+                        "VirusShare_98eb1f31945f4cd97088cf9fbc49d03b",
+                        "VirusShare_aecb7c76cb497401be48459ff944f5fe",
+                        "VirusShare_bcb3026536783bc774a05d93bc2f6039",
+                        "VirusShare_c69d0d8b86bf3946ccbc011767b06919",
+                        "VirusShare_ff28b758f18030c14402e100dbb6987e");
                 for (String black : blackList) {
                     if (fileName.equals(black)) {
                         return false;
@@ -128,24 +111,24 @@ public class CFG {
     // constructCFG constructs CallFlowGraph and write the result as JSON
     public void constructCFG() {
         List<CFGConstructor> jobs = new ArrayList<>();
-        int allApksNum = benignApks.length+maliciousApks.length;
+        int allApksNum = benignApks.length + maliciousApks.length;
         System.out.printf("All Apks: %d\n", allApksNum);
-        for (int idx=apk_lb; idx < apk_ub; ++idx) {
-            if (idx < benignApks.length) {
-                jobs.add(new CFGConstructor(idx, benignApks[idx], 0));
+        for (int idx = apk_lb; idx < apk_ub; ++idx) {
+            if (idx < maliciousApks.length) {
+                jobs.add(new CFGConstructor(idx, maliciousApks[idx], 1));
             } else {
-                jobs.add(new CFGConstructor(idx, maliciousApks[idx-benignApks.length], 1));
+                jobs.add(new CFGConstructor(idx, benignApks[idx - maliciousApks.length], 0));
             }
         }
         jobs.stream().forEach(job -> {
             ReturnedValue res = job.call();
             Map<String, Integer> apiFreq = res.getApiFreq();
             apiFreq.put(familyKey, res.getFamily());
-            this.apiFreqs.set(res.getIdx()-apk_lb, apiFreq);
+            this.apiFreqs.set(res.getIdx() - apk_lb, apiFreq);
 
             List<String> apiSequence = res.getApiSequence();
             apiSequence.add(String.valueOf(res.getFamily()));
-            this.apiSequences.set(res.getIdx()-apk_lb, apiSequence);
+            this.apiSequences.set(res.getIdx() - apk_lb, apiSequence);
         });
 
         this.deriveAllApis();
@@ -193,16 +176,19 @@ public class CFG {
             ObjectMapper objectMapper = new ObjectMapper();
 
             // write apiFreq
-            writeJSONWithFileName(objectMapper, apiFrequenciesFilePath+"."+String.valueOf(this.chunk_i), apiFreqs);
+            writeJSONWithFileName(objectMapper, apiFrequenciesFilePath + "." + String.valueOf(this.chunk_i), apiFreqs);
             // write allApiSets
-            writeJSONWithFileName(objectMapper, allApisFilePath+"."+String.valueOf(this.chunk_i), allApis);
+            writeJSONWithFileName(objectMapper, allApisFilePath + "." + String.valueOf(this.chunk_i), allApis);
 
             // NOTE: As this log file is easilly big, commented out
             // write apiSequences
-            writeJSONWithFileName(objectMapper, apiSequencesFilePath+"."+String.valueOf(this.chunk_i), apiSequences);
+            writeJSONWithFileName(objectMapper, apiSequencesFilePath + "." + String.valueOf(this.chunk_i),
+                    apiSequences);
 
             // write apiSequences coverted with index
-            // writeJSONWithFileName(objectMapper, apiSequenceIndicesFilePath+"."+String.valueOf(this.chunk_i), apiSequenceIndices);
+            // writeJSONWithFileName(objectMapper,
+            // apiSequenceIndicesFilePath+"."+String.valueOf(this.chunk_i),
+            // apiSequenceIndices);
         } catch (Exception e) {
             e.printStackTrace();
         }
